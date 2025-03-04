e9eaf6b5132734959b409d6eaf2ee97b8cfd28ef
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-03-04 10:58:05 -0500

    Update ContentView.swift

--- AnimationNotesApp/ContentView.swift
+++ AnimationNotesApp/ContentView.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
+    @State private var chevronRotate = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button("", systemImage: "chevron.right") {
+                    withAnimation {
+                        showDetails.toggle()
+                        if showDetails {
+                            chevronRotate = 90.0
+                        }
+                        else {
+                            chevronRotate = 0.0
+                        }
+                    }
+
+
+                }
+                .rotationEffect(.degrees(chevronRotate))
+                
+                Text("Animation Notes")
+                    .font(.headline)
+                Spacer()
+            }
+            
+            if showDetails {
+                Text("blah")
+            }
+            
+            Spacer()
+            
         }
         .padding()
     }
