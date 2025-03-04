6d3aeddcc7c4fefa8d7252a6ba2fb19408d24fb4
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-03-04 10:35:56 -0500

    Initial Commit

---
+++ AnimationNotesApp/AnimationNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationNotesAppApp.swift
+//  AnimationNotesApp
+//
+//  Created by Ming Bian on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationNotesAppTests/AnimationNotesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnimationNotesAppTests.swift
+//  AnimationNotesAppTests
+//
+//  Created by Ming Bian on 3/4/25.
+//
+
+import Testing
+
+struct AnimationNotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
