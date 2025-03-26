aa773a1c678aa22e691932f7756bdb27be784415
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-03-26 09:55:57 -0400

    minor changes

--- AnimationNotesApp/ContentView.swift
+++ AnimationNotesApp/ContentView.swift
@@ -10,39 +10,45 @@ import SwiftUI
 struct ContentView: View {
     @State private var showDetails = false
     @State private var chevronRotate = 0.0
+    @State private var opacity = 2.0
+    enum AnimationPhase: CaseIterable {
+        case start, middle, end
+    }
     
     var body: some View {
         VStack (alignment: .leading, spacing: 10) {
             HStack {
                 Button("", systemImage: "chevron.right") {
-                    withAnimation {
+                    withAnimation(.linear) {
                         showDetails.toggle()
                         if showDetails {
-                            chevronRotate = 90.0
+                            chevronRotate += 90.0
                         }
                         else {
-                            chevronRotate = 0.0
+                            chevronRotate -= 90.0
                         }
                     }
-
-
+                    
                 }
                 .rotationEffect(.degrees(chevronRotate))
                 
                 Text("Animation Notes")
                     .font(.headline)
+                
                 Spacer()
             }
             
             if showDetails {
-                Text("There are two primary types of animations in SwiftUI: explicit and implicit. Explicit are primarily done with `withAnimation`, while implicit are done with `animation()` *Emphasis*")
+                Text("There are two primary types of animations in SwiftUI: explicit and implicit. Explicit are primarily done with `withAnimation`, while implicit are done with `animation()`")
+                    .onTapGesture {
+                        withAnimation {
+                            opacity -= 0.3
+                        }
+                    }
+                    .opacity(opacity)
+
             }
             
-            HStack {
-                Text("more notes")
-                    .font(.headline)
-                Spacer()
-            }
             
             Spacer()
             
